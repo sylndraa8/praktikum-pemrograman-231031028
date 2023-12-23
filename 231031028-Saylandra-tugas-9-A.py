5ee379e7ed070c91d2547fd8410a33c0fb004aec
sylndraa8 <153787927+sylndraa8@users.noreply.github.com>
2023-12-23 09:12:26 +0800

    Update 231031028-Saylandra-tugas-9-A.py

--- 231031028-Saylandra-tugas-9-A.py
+++ 231031028-Saylandra-tugas-9-A.py
@@ -1,20 +1,19 @@
-Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> data= {'nama lengkap'        : 'Saylandra Aulia Ramadhani',
-...        'nama panggilan '     : 'saylandra',
-...        'tempat/tanggal lahir': 'parepare, 8 Oktober 2005',
-...        'alamat'              : 'Perum. Griya Lapadde Indah',
-...        'agama'               : 'Islam',
-...        'umur'                : '18 Tahun',
-...        'jenis kelamin'       : 'Perempuan',
-...        'tinggi badan'        : '158',
-...        'nomor hp'            : '087846537274',
-...        'program studi'       : 'Sistem Informasi',
-...        'angkatan'            : '2023' 
-...         }
-... 
-... print('Nama                 :', data['nama lengkap'])
-... print('Tempat/Tanggal Lahir :', data['tempat/tanggal lahir'])
-... print('Jenis kelamin        :', data['jenis kelamin'])
-... print('Agama                :', data['agama'])
-... print('Program Studi        :', data['program studi'])
+Biodata = {'nama lengkap'        : 'Saylandra Aulia Ramadhani',
+           'nama panggilan'     : 'saylandra',
+           'tempat/tanggal lahir': 'parepare, 8 Oktober 2005',
+        'alamat'              : 'Perum. Griya Lapadde Indah',
+        'agama'               : 'Islam',
+        'umur'                : '18 Tahun',
+        'jenis kelamin'       : 'Perempuan',
+        'tinggi badan'        : '158',
+        'nomor hp'            : '087846537274',
+        'program studi'       : 'Sistem Informasi',
+        'angkatan'            : '2023' 
+         }
+
+print(f'''
+Nama Lengkap\t: {Biodata['nama lengkap']}
+TTl\t: {Biodata['tempat/tanggal lahir']}
+Alamat\t: {Biodata['alamat']}
+Agama\t: {Biodata['agama']}
+''')
